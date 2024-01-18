0d7be07f08fa77240c9dcdc3f8b3a1dc008d6035
altink7 <kelmendialtin48@gmail.com>
2024-01-18 19:45:17 +0100

    testing virtual threads

--- src/main/java/at/altin/loginspring/java21/Main2.java
+++ src/main/java/at/altin/loginspring/java21/Main2.java
@@ -15,13 +15,25 @@ void main(String[] args) {
         //when only the normal thread method is called, the time is longer
         testNormalThread(numberOfThreads);
 
-        //Example:
-        //Virtual Thread Performance: 570 ms
+        //Run 1:
+        //Virtual Thread Performance: 570 ms (waiting time for normal thread method)
         //Normal Thread Performance: 76 ms
         //Virtual Thread Performance: 6 ms
         //Normal Thread Performance: 23 ms
         //
         //Process finished with exit code 0
+
+        //Run 2:
+        //Virtual Thread Performance: 544 ms (waiting time for normal thread method)
+        //Normal Thread Performance: 70 ms
+        //Virtual Thread Performance: 7 ms
+        //Normal Thread Performance: 24 ms
+        //
+        //Process finished with exit code 0
+
+
+        //till the Virtual Thread System is triggered to wait also the normal Thread is blocked and the time is longer
+        // difference = 76 - 23 = 53 ms and 70 - 24 = 46 ms
 }
 
 
